076a23ba43d093b2cb117df62fe75aaf0c2a29b0
Rafael <rafael.pereira3465@gmail.com>
2024-05-31 13:58:47 +1000

    Messanging

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -157,9 +157,31 @@ loadMessages = () => {
   // Start listening to the query.
   return collectionData(recentMessagesQuery);
 }
-  // Saves a new message containing an image in Firebase.
-  // This first saves the image in Firebase storage.
-  saveImageMessage = async (file: any) => {};
+// Saves a new message containing an image in Firestore.
+// This first saves the image in Firebase storage.
+saveImageMessage = async(file: any) => {
+  try {
+    // 1 - Add a message with a loading icon that will get updated with the shared image.
+    const messageRef = await this.addMessage(null, this.LOADING_IMAGE_URL);
+
+    // 2 - Upload the image to Cloud Storage.
+    const filePath = `${this.auth.currentUser?.uid}/${file.name}`;
+    const newImageRef = ref(this.storage, filePath);
+    const fileSnapshot = await uploadBytesResumable(newImageRef, file);
+
+    // 3 - Generate a public URL for the file.
+    const publicImageUrl = await getDownloadURL(newImageRef);
+
+    // 4 - Update the chat message placeholder with the image's URL.
+    messageRef ?
+    await updateDoc(messageRef, {
+      imageUrl: publicImageUrl,
+      storageUri: fileSnapshot.metadata.fullPath
+    }): null;
+  } catch (error) {
+    console.error('There was an error uploading a file to Cloud Storage:', error);
+  }
+}
 
   async updateData(path: string, data: any) {}
 
@@ -176,8 +198,44 @@ loadMessages = () => {
   ) {
     return null;
   }
-  // Requests permissions to show notifications.
-  requestNotificationsPermissions = async () => {};
+ // Requests permissions to show notifications.
+requestNotificationsPermissions = async () => {
+  console.log('Requesting notifications permission...');
+  const permission = await Notification.requestPermission();
+  
+  if (permission === 'granted') {
+    console.log('Notification permission granted.');
+    // Notification permission granted.
+    await this.saveMessagingDeviceToken();
+  } else {
+    console.log('Unable to get permission to notify.');
+  }
+}
 
-  saveMessagingDeviceToken = async () => {};
+ // Saves the messaging device token to Cloud Firestore.
+saveMessagingDeviceToken= async () => {
+  try {
+    const currentToken = await getToken(this.messaging);
+    if (currentToken) {
+      console.log('Got FCM device token:', currentToken);
+      // Saving the Device Token to Cloud Firestore.
+      const tokenRef = doc(this.firestore, 'fcmTokens', currentToken);
+      await setDoc(tokenRef, { uid: this.auth.currentUser?.uid });
+
+      // This will fire when a message is received while the app is in the foreground.
+      // When the app is in the background, firebase-messaging-sw.js will receive the message instead.
+      onMessage(this.messaging, (message) => {
+        console.log(
+          'New foreground notification from Firebase Messaging!',
+          message.notification
+        );
+      });
+    } else {
+      // Need to request permissions to show notifications.
+      this.requestNotificationsPermissions();
+    }
+  } catch(error) {
+    console.error('Unable to get messaging token.', error);
+  };
+}
 }
